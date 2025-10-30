46a85fa5d0416a91d338328f0ddc5bd582576028
Ronit Kapoor <ronitkapoor@Ronits-MacBook-Air-1108.local>
2025-10-30 16:57:14 -0400

    fixes

--- profile.py
+++ profile.py
@@ -10,7 +10,6 @@ pc.defineParameter("nodeType", "Hardware type",
                    portal.ParameterType.STRING, "c6525-25g",
                    [("c6525-25g", "25G Mellanox"),
                     ("d6515",     "25G Mellanox")])
-# Use STRING for broad compatibility (IMAGE can be flaky on some portals)
 pc.defineParameter("osImage", "OS Image",
                    portal.ParameterType.STRING,
                    "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU24-64-STD")
@@ -18,17 +17,16 @@ pc.defineParameter("osImage", "OS Image",
 params = pc.bindParameters()
 req = pc.makeRequestRSpec()
 
-lan = pg.LAN("lan")
+lan = req.LAN("lan")
 lan.vlan_tagging = False
 lan.link_multiplexing = False
-lan.bandwidth = 25 * 1000 * 1000  # ~25 Gbps in Kbps units
+lan.bandwidth = 25 * 1000 * 1000
 
 for i in range(params.nodeCount):
-    n = pg.RawPC("node%d" % i)
+    n = req.RawPC("node%d" % i)
     n.hardware_type = params.nodeType
     n.disk_image = params.osImage
 
-    # Pass a role to startup.sh (node0=server, others=client)
     role = "server" if i == 0 else "client"
     n.addService(pg.Execute(shell="bash",
                             command="/local/repository/startup.sh %s" % role))
@@ -37,6 +35,4 @@ for i in range(params.nodeCount):
     iface.addAddress(pg.IPv4Address("10.10.1.%d" % (i + 1), "255.255.255.0"))
     lan.addInterface(iface)
 
-
-req.addResource(lan)
 pc.printRequestRSpec(req)
